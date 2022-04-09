c91d7747379af674cf2f640138ada6b4b6469878
euquias <willmagalhaes25@gmail.com>
2022-04-09 09:35:01 -0300

    adiconando component bory

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { BoryComponent } from './components/template/bory/bory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/template/bory/bory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bory',
+  templateUrl: './bory.component.html',
+  styleUrls: ['./bory.component.css']
+})
+export class BoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
