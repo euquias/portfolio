acfd8f07feb6feeba67a8af340142c4e16274eda
euquias <willmagalhaes25@gmail.com>
2022-04-09 11:08:28 -0300

    adiconando component footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { BoryComponent } from './components/template/bory/bory.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BoryComponent
+    BoryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
