87b0c3807f525761287ee216a230114079008c7e
euquias <willmagalhaes25@gmail.com>
2022-07-11 16:25:38 -0300

    problema referente ao component dashboard

--- frontend/src/app/components/template/bory/dashboard/dashboard.component.ts
+++ frontend/src/app/components/template/bory/dashboard/dashboard.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  const couter = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  setInterval(() =>{
+    const el =
+    document.querySelector('number');
+    const elvalue=
+    Number(el.getAttribute('data-value'));
+    if(couter !==elvalue){
+      couter++;
+      el.innerHTML = ´${couter}%´;
+    }
+  
+  }, 80);
 
 }
